1b4d9f2e15e220a03a85bdb30463fb7634e88e3c
Natasya3101 <natasyaalvngu@gmail.com>
2024-08-28 00:26:08 +0700

    first commit

---
+++ be/src/test/java/com/parfumaria/be/BeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.parfumaria.be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
